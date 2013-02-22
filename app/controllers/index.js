2d16e74ac55d7a76666f1375ae5bfc9e57f29a9d
Joe Longstreet <joelongstreet@gmail.com>
2013-02-21 18:21:06 -0600

    base value updates

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 // Setup Default Values
 Alloy.Globals.defaults = {
-    'Target Gravity'  : 1.040,
-    'Actual Gravity'  : 1.040,
+    'Target Gravity'  : 1.054,
+    'Actual Gravity'  : 1.048,
     'Temperature'     : 100,
     'Volume'          : 5.5
 };
